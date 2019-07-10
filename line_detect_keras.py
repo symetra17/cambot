c55b589fe6f7939e61925fcd55c5d99cc3f44627
nvidia <chikman428@gmail.com>
2019-07-10 12:21:33 +0800

    gpu memory allocation allow_growth

--- line_detect_keras.py
+++ line_detect_keras.py
@@ -16,6 +16,14 @@ from keras.layers import Conv2D, MaxPooling2D
 from keras.optimizers import SGD
 import time
 
+from keras.backend.tensorflow_backend import set_session
+import tensorflow as tf
+config = tf.ConfigProto()
+config.gpu_options.allow_growth = True  # dynamically grow the memory used on the GPU
+
+sess = tf.Session(config=config)
+set_session(sess)  # set this TensorFlow session as the default session for Keras
+
 model = Sequential()
 template = None
 diff_thd = 7.0    # the lower this value, the more sensitive
@@ -107,3 +115,4 @@ if __name__=='__main__':
     #detect(img)
     for fn in glob.glob('samples/*.bmp'):
         detect(fn)
+        input('press enter')
