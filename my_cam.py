4cb31a9be6e9da0b8b0bd0e315b9d8f26a0f731b
nvidia <chikman428@gmail.com>
2019-07-10 11:38:22 +0800

    added save images

--- my_cam.py
+++ my_cam.py
@@ -1,5 +1,6 @@
 import cv2
 import os
+import time
 
 def cam_init():
     cap = cv2.VideoCapture(0)
@@ -10,3 +11,23 @@ def cam_init():
     for n in range(10):
         ret, frame = cap.read()
     return cap
+
+if __name__== '__main__':
+    cap=cam_init()
+    n=0
+    while True:
+      for m in range(6):
+          cap.read()
+      ret, im=cap.read()
+      im=cv2.rotate(im, cv2.ROTATE_90_CLOCKWISE)
+      im=im[280:-280,:,:]
+      cv2.imshow('opencv imshow', im)
+      kee = cv2.waitKey(10)
+      if kee==ord('q'):
+          quit()
+      elif kee==ord(' '):
+          print 'write %d'%n
+          cv2.imwrite('im_%04d.bmp'%n, im)
+          n=n+1
+      time.sleep(0.5)
+
